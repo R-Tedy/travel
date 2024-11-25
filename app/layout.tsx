b0b560a16c8aac228ab8bbd2715e5d5b26755163
Roland Stedy <stedyroland@gmail.com>
2024-11-26 02:31:30 +0300

    title update

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: "Travel App",
+  title: "Stedy Run",
   description: "A travel website model app",
 };
 
